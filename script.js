1eeab8d992dc180106619f1837fc621cbb817612
unknown <Zama Gcukumana>
2020-10-10 20:21:07 +0200

    Week 4: Add Two Numbers

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ function addNumbers() {
 	 let x = parseFloat(document.getElementById('number1').value);
 	 let y = parseFloat(document.getElementById('number2').value);
 	//PROCESSING
+	//Convert the input strings to numbers using parsefloat;
 	 let sum = x + y;
+	 //Add the two numbers together and store in my output variable
 	//OUTPUT
 	 document.getElementById('output').innerHTML = "The sum of " + x + " and " + y + " equals " + sum + "!";
 }
